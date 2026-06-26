5ec42059f3136d33c36d20da0927cfc20fb7e679
van456-dot <vansh27319@gmail.com>
2026-06-26 10:19:28 +0530

    load on submit

--- client/src/components/MultiStepForm.jsx
+++ client/src/components/MultiStepForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
 function MultiStepForm({ onClose }) {
     const [step, setStep] = useState(1);
